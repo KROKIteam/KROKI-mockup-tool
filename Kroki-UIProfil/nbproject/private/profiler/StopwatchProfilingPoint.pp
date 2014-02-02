1d1c399899ef86a95f7c57d51a300b67ad20af72
Milorad Filipovic <miloradfilipovic19@gmail.com>
2014-02-02 22:38:36 +0100

    Removed config files

--- Kroki-UIProfil/nbproject/private/profiler/StopwatchProfilingPoint.pp
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
-<properties>
-<entry key="0_start_p_location_line">64</entry>
-<entry key="0_start_p_location_offset">-2147483648</entry>
-<entry key="0_start_p_location_file">{$projectDirectory}/src/kroki/profil/panel/VisibleClass.java</entry>
-<entry key="0_p_enabled">true</entry>
-<entry key="0_p_name">Stopwatch at VisibleClass:109</entry>
-</properties>
--- KrokiMockupTool/build/classes/kroki/app/generators/templates/database.properties.ftl
+++
@@ -1,4 +0,0 @@
-driver=${driver}
-url=jdbc:${protocol}://${host}:{port}/{schema}
-username=${username}
-password=${password}
