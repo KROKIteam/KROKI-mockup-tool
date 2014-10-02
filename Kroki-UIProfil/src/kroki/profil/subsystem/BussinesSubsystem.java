d6f9bd67340648f07af461996a9e2bd08a00d58d
Renata <vrenata8@gmail.com>
2014-10-02 07:54:44 +0200

    Mainteining alphabetical order when adding panels and packages in mockup editor, see #60
    Tree stays sorted so that, inside a project, packages are shown first, sorted aplabetically, followed by the sorted panels.

--- Kroki-UIProfil/src/kroki/profil/subsystem/BussinesSubsystem.java
+++ Kroki-UIProfil/src/kroki/profil/subsystem/BussinesSubsystem.java
@@ -11,10 +11,8 @@ import java.util.List;
 import kroki.profil.ComponentType;
 import kroki.profil.VisibleElement;
 import kroki.profil.association.VisibleAssociationEnd;
-import kroki.profil.association.Zoom;
 import kroki.profil.panel.StandardPanel;
 import kroki.profil.panel.VisibleClass;
-import kroki.profil.panel.container.ParentChild;
 import kroki.profil.utils.DatabaseProps;
 import kroki.profil.utils.visitor.AllPosibleHierarchyPanels;
 import kroki.profil.utils.visitor.AllPosibleNextPanels;
@@ -71,7 +69,7 @@ public class BussinesSubsystem extends VisibleElement implements UmlPackage {
 		if (!ownedType.contains(umlType)) {
 			ownedType.add(umlType);
 			umlType.setUmlPackage(this);
-			ownedElement.add((VisibleElement) umlType);
+			sortedInsert((VisibleElement) umlType);
 		}
 	}
 
@@ -86,7 +84,7 @@ public class BussinesSubsystem extends VisibleElement implements UmlPackage {
 		if (!nestedPackage.contains(umlPackage)) {
 			nestedPackage.add(umlPackage);
 			umlPackage.setNestingPackage(this);
-			ownedElement.add((VisibleElement) umlPackage);
+			sortedInsert((VisibleElement) umlPackage);
 		}
 	}
 
@@ -96,6 +94,25 @@ public class BussinesSubsystem extends VisibleElement implements UmlPackage {
 			ownedElement.remove((VisibleElement) umlPackage);
 		}
 	}
+
+	private void sortedInsert(VisibleElement element){
+		for (int i = 0; i < ownedElementCount(); i++){
+			VisibleElement el = ownedElement.get(i);
+			if (element.getClass() != el.getClass()){
+				if (element instanceof UmlPackage){
+					ownedElement.add(i, element);
+					return;
+				}
+				continue;
+			}
+			if (el.getLabel().toLowerCase().compareTo(element.getLabel().toLowerCase()) > 0){
+				ownedElement.add(i, element);
+				return;
+			}
+		}
+		ownedElement.add(element);
+	}
+
 
 	public List<UmlPackage> nestedPackage() {
 		return nestedPackage;
@@ -136,21 +153,21 @@ public class BussinesSubsystem extends VisibleElement implements UmlPackage {
 		allAssociationEnds(this, ret);
 		return ret;
 	}
-	
+
 
 	public List<VisibleClass> allPanels(){
 		ArrayList<VisibleClass> ret = new ArrayList<VisibleClass>();
 		allPanels(this, ret);
 		return ret;
 	}
-	
+
 
 	/**
 	 * Finds all visible association ends which are contained by the subsystem and its nested packages
 	 * @param pack
 	 * @param ret
 	 */
-	 protected void allAssociationEnds(BussinesSubsystem pack, List<VisibleAssociationEnd> ret){
+	protected void allAssociationEnds(BussinesSubsystem pack, List<VisibleAssociationEnd> ret){
 		for (UmlType ownedType : pack.ownedType()){
 			if (!(ownedType instanceof VisibleClass))
 				continue;
@@ -160,69 +177,69 @@ public class BussinesSubsystem extends VisibleElement implements UmlPackage {
 		}
 		for (UmlPackage ownedPackage : pack.nestedPackage())
 			allAssociationEnds((BussinesSubsystem) ownedPackage, ret);
-	 }
+	}
 
-	 protected void allPanels(BussinesSubsystem pack, List<VisibleClass> ret){
+	protected void allPanels(BussinesSubsystem pack, List<VisibleClass> ret){
 		for (UmlType ownedType : pack.ownedType()){
 			if ((ownedType instanceof VisibleClass))
 				ret.add((VisibleClass) ownedType);
 		}
 		for (UmlPackage ownedPackage : pack.nestedPackage())
 			allPanels((BussinesSubsystem) ownedPackage, ret);
-	 }
+	}
 
-	 
 
-	 /****************************************************/
-	 /*IMPLEMENTIRANE METODE INTERFEJSA VisitingSubsystem*/
-	 /****************************************************/
-	 public void accept(AllPosibleZoomPanels visitor) {
-		 visitor.addAllObjects(ownedType);
-		 for (int i = 0; i < nestedPackage.size(); i++) {
-			 BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
-			 subsystem.accept(visitor);
-		 }
-	 }
 
-	 public void accept(AllPosibleNextPanels visitor) {
-		 visitor.addAllObjects(ownedType);
-		 for (int i = 0; i < nestedPackage.size(); i++) {
-			 BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
-			 subsystem.accept(visitor);
-		 }
-	 }
+	/****************************************************/
+	/*IMPLEMENTIRANE METODE INTERFEJSA VisitingSubsystem*/
+	/****************************************************/
+	public void accept(AllPosibleZoomPanels visitor) {
+		visitor.addAllObjects(ownedType);
+		for (int i = 0; i < nestedPackage.size(); i++) {
+			BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
+			subsystem.accept(visitor);
+		}
+	}
 
-	 public void accept(AllPosibleNexts visitor) {
-		 for (UmlType owned : ownedType)
-			 if (owned instanceof StandardPanel)
-				 visitor.addAllObjects(((StandardPanel)owned).containedZooms());
+	public void accept(AllPosibleNextPanels visitor) {
+		visitor.addAllObjects(ownedType);
+		for (int i = 0; i < nestedPackage.size(); i++) {
+			BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
+			subsystem.accept(visitor);
+		}
+	}
 
-		 for (int i = 0; i < nestedPackage.size(); i++) {
-			 BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
-			 subsystem.accept(visitor);
-		 }
-	 }
+	public void accept(AllPosibleNexts visitor) {
+		for (UmlType owned : ownedType)
+			if (owned instanceof StandardPanel)
+				visitor.addAllObjects(((StandardPanel)owned).containedZooms());
 
-	 public void accept(AllPosibleHierarchyPanels visitor) {
-		 visitor.addAllObjects(ownedType);
-		 for (int i = 0; i < nestedPackage.size(); i++) {
-			 BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
-			 subsystem.accept(visitor);
-		 }
-	 }
+		for (int i = 0; i < nestedPackage.size(); i++) {
+			BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
+			subsystem.accept(visitor);
+		}
+	}
 
-	 public void accept(ContainingPanels visitor) {
-		 visitor.addAllObjects(ownedType);
-		 for (int i = 0; i < nestedPackage.size(); i++) {
-			 BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
-			 subsystem.accept(visitor);
-		 }
-	 }
+	public void accept(AllPosibleHierarchyPanels visitor) {
+		visitor.addAllObjects(ownedType);
+		for (int i = 0; i < nestedPackage.size(); i++) {
+			BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
+			subsystem.accept(visitor);
+		}
+	}
 
-	 @Override
-	 public String toString() {
-		 return label;
-	 }
+	public void accept(ContainingPanels visitor) {
+		visitor.addAllObjects(ownedType);
+		for (int i = 0; i < nestedPackage.size(); i++) {
+			BussinesSubsystem subsystem = (BussinesSubsystem) nestedPackage.get(i);
+			subsystem.accept(visitor);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return label;
+	}
 
 	public Object getGraphPackage() {
 		return graphPackage;
