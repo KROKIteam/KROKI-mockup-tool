f14d306b1ab1fdeb41300770099d0135ad90380d
Renata <vrenata8@gmail.com>
2014-11-28 11:13:31 +0100

    Paste command fix

--- KrokiMockupTool/src/kroki/app/command/PasteCommand.java
+++ KrokiMockupTool/src/kroki/app/command/PasteCommand.java
@@ -1,20 +1,22 @@
 package kroki.app.command;
 
-import graphedit.util.SerializationUtility;
-
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import kroki.app.KrokiMockupToolApp;
 import kroki.app.model.SelectionModel;
 import kroki.app.view.Canvas;
 import kroki.common.copy.DeepCopy;
-import kroki.commons.camelcase.NamingUtil;
 import kroki.profil.VisibleElement;
-import kroki.profil.association.Next;
+import kroki.profil.association.VisibleAssociationEnd;
 import kroki.profil.group.ElementsGroup;
 import kroki.profil.panel.VisibleClass;
 import kroki.profil.property.VisibleProperty;
+import kroki.uml_core_basic.UmlProperty;
+import kroki.uml_core_basic.UmlType;
+import kroki.uml_core_basic.UmlTypedElement;
 
 public class PasteCommand implements Command {
 
@@ -24,12 +26,20 @@ public class PasteCommand implements Command {
     private Point point;
     private int classIndex, groupIndex;
     private boolean cutAction;
+    private Map<UmlProperty, UmlProperty> oppositeMap = new HashMap<UmlProperty, UmlProperty>();
+    private Map<UmlTypedElement, UmlType> typeMap = new HashMap<UmlTypedElement, UmlType>();
+    private Map<VisibleElement, VisibleElement> copies = new HashMap<VisibleElement, VisibleElement>();
+    private Map<VisibleAssociationEnd, VisibleClass> targetPanelsMap = new HashMap<VisibleAssociationEnd, VisibleClass>();
 	
-    @SuppressWarnings("unchecked")
 	public PasteCommand(VisibleClass visibleClass, ElementsGroup elementsGroup, List<VisibleElement> elements, Point point, boolean cutAction) {
         this.visibleClass = visibleClass;
         this.elementsGroup = elementsGroup;
-        this.elements = (List<VisibleElement>) DeepCopy.copy(elements);
+        for (VisibleElement el : elements){
+        	saveAndRemoveAttributes(el);
+        	copies.put((VisibleElement)DeepCopy.copy(el), el);
+        	restoreAttributes(el, el);
+        }
+        	
         this.point = point;
         this.cutAction = cutAction;
         classIndex = visibleClass.getVisibleElementList().size();
@@ -38,7 +48,8 @@ public class PasteCommand implements Command {
 	
 	@Override
 	public void doCommand() {
-		for (VisibleElement element : elements) {
+		for (VisibleElement element : copies.keySet()) {
+			restoreAttributes(copies.get(element), element);
 			visibleClass.addVisibleElement(classIndex, element);
 	        elementsGroup.addVisibleElement(groupIndex, element);
 	        
@@ -78,6 +89,59 @@ public class PasteCommand implements Command {
             if (element.getComponentType() != null){
 				visibleClass.decrementCount(element.getComponentType());
 			}
+		}
+	}
+	
+	
+	private void saveAndRemoveAttributes(VisibleElement el){
+		
+		if (el instanceof UmlProperty){
+			UmlProperty prop = (UmlProperty)el;
+			oppositeMap.put(prop, prop.opposite());
+			prop.setOpposite(null);
+		}
+		
+		if (el instanceof UmlTypedElement){
+			UmlTypedElement typed = (UmlTypedElement) el;
+			typeMap.put(typed, typed.type());
+			typed.setType(null);
+		}
+		
+		if (el instanceof VisibleAssociationEnd){
+			VisibleAssociationEnd end = (VisibleAssociationEnd) el;
+			targetPanelsMap.put(end, end.getTargetPanel());
+			end.setTargetPanel(null);
+			end.setActivationPanel(null);
+		}
+		
+		if (el instanceof VisibleProperty){
+			VisibleProperty prop = (VisibleProperty) el;
+			prop.setUmlClass(null);
+		}
+	}
+	
+	private void restoreAttributes(VisibleElement keyEl, VisibleElement targetEl){
+		
+		if (targetEl instanceof UmlProperty){
+			UmlProperty prop = (UmlProperty)targetEl;
+			prop.setOpposite(oppositeMap.get(keyEl));
+		}
+		
+		if (targetEl instanceof UmlTypedElement){
+			UmlTypedElement typed = (UmlTypedElement) targetEl;
+			typed.setType(typeMap.get(keyEl));
+		}
+		
+		if (targetEl instanceof VisibleAssociationEnd){
+			VisibleAssociationEnd end = (VisibleAssociationEnd) targetEl;
+			end.setTargetPanel(targetPanelsMap.get(keyEl));
+			end.setActivationPanel(visibleClass);
+		}
+		
+		if (targetEl instanceof VisibleProperty){
+			VisibleProperty prop = (VisibleProperty) targetEl;
+			if (prop.umlClass() != null)
+				prop.setUmlClass(visibleClass);
 		}
 	}
 
