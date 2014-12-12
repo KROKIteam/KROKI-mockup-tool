664411647da00a9e1c1c3d4a67b2c690d478cecd
Sebastijan Kaplar <sebastijan.kaplar@gmail.com>
2014-12-12 17:05:49 +0100

    Added support for classes for server and deployment options.

--- WebApp/src/adapt/core/AdaptMainFrame.java
+++ WebApp/src/adapt/core/AdaptMainFrame.java
@@ -28,95 +28,25 @@ public class AdaptMainFrame {
 
 	public static String DEPLOY_SERVER = "server";
 	public static String DEPLOY_DEVELOP = "develop";
-	//	private JScrollPane scrollPane;
-	//	private JTextPane statusPane;
 	Component component;
-
+	AdaptMainFrameDevelop amfd = null;
 	public AdaptMainFrame() {
 		component = new Component();
 		if (Settings.DEPLOY.equals(DEPLOY_DEVELOP)) {
-			//			init();
-			//			setTitle(Settings.APP_TITLE + " [Server log]");
-			//			setMaximumSize(new Dimension(850, 400));
-			//			setMinimumSize(new Dimension(850, 400));
-			//			setLocationRelativeTo(null);
-			//			setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			//			loadIcon();
-			//			setVisible(true);
+			amfd = new AdaptMainFrameDevelop(component);
+			amfd.setVisible(true);
 		}
 		try {
 			runApp();
 			displayText("Server running...", 0);
+			if(amfd != null) {
+				amfd.navigate();
+			}
 		} catch (Exception e1) {
 			e1.printStackTrace();
 			displayStackTrace(e1);
 		}
 	}
-
-	//	private void init() {
-	//		scrollPane = new JScrollPane();
-	//		statusPane = new JTextPane();
-	//		statusPane.setEditable(false);
-	//		statusPane.setFont(new Font("Monospaced",Font.PLAIN,12));
-	//		statusPane.setForeground(Color.white);
-	//		statusPane.setBackground(Color.black);
-	//
-	//		AdaptMainFrame.this.addWindowListener(new WindowListener() {
-	//			@Override
-	//			public void windowOpened(WindowEvent arg0) {
-	//			}
-	//
-	//			@Override
-	//			public void windowIconified(WindowEvent arg0) {
-	//			}
-	//
-	//			@Override
-	//			public void windowDeiconified(WindowEvent arg0) {
-	//			}
-	//
-	//			@Override
-	//			public void windowDeactivated(WindowEvent arg0) {
-	//			}
-	//
-	//			@Override
-	//			public void windowClosing(WindowEvent arg0) {
-	//				try {
-	//					component.stop();
-	//					System.exit(ABORT);
-	//				} catch (Exception e) {
-	//					e.printStackTrace();
-	//					JOptionPane.showMessageDialog(null, e.getMessage());
-	//					System.exit(ABORT);
-	//				}
-	//			}
-	//
-	//			@Override
-	//			public void windowClosed(WindowEvent arg0) {
-	//			}
-	//
-	//			@Override
-	//			public void windowActivated(WindowEvent arg0) {
-	//			}
-	//		});
-	//
-	//		scrollPane.setViewportView(statusPane);
-	//		add(scrollPane, BorderLayout.CENTER);
-	//
-	//		try {
-	//			runApp();
-	//			displayText("Server running...", 0);
-	//		} catch (Exception e1) {
-	//			e1.printStackTrace();
-	//			displayStackTrace(e1);
-	//		}
-	//		try {
-	//			Desktop.getDesktop().browse(new URI("http://localhost:8182/"));
-	//		} catch (IOException e) {
-	//			e.printStackTrace();
-	//		} catch (URISyntaxException e) {
-	//			e.printStackTrace();
-	//		}
-	//	}
 
 	public void runApp() throws Exception {
 		component.getServers().add(Protocol.HTTP, 8182);
@@ -183,31 +113,9 @@ public class AdaptMainFrame {
 		if (Settings.DEPLOY.equals(DEPLOY_SERVER)) {
 			System.out.println(text);
 			return;
+		} else if (Settings.DEPLOY.equals(DEPLOY_DEVELOP)){
+			amfd.displayText(text, type);
 		}
-
-//		StyledDocument document = statusPane.getStyledDocument();
-//		SimpleAttributeSet set = new SimpleAttributeSet();
-//		statusPane.setCharacterAttributes(set, true);
-//		SimpleDateFormat formatter = new SimpleDateFormat(Settings.DATE_TIME_SECONDS_FORMAT);
-//		Date today = new Date();
-//		String prefix = "[" + formatter.format(today) + "] ";
-//		StyleConstants.setForeground(set, Color.white);
-//		if(type == 1) {
-//			StyleConstants.setForeground(set, Color.red);
-//			prefix += "[ERROR] ";
-//		}
-//		if(type == 2) {
-//			StyleConstants.setForeground(set, Color.yellow);
-//			prefix += "[WARINIG] ";
-//		}
-//		try {
-//			document.insertString(document.getLength(), prefix + text + "\n", set);
-//		} catch (BadLocationException e) {
-//			e.printStackTrace();
-//		}
-//		statusPane.setCaretPosition(statusPane.getDocument().getLength());
-//		JScrollBar vertical = scrollPane.getVerticalScrollBar();
-//		vertical.setValue( vertical.getMaximum() );
 	}
 
 	/**
@@ -218,22 +126,8 @@ public class AdaptMainFrame {
 		if (Settings.DEPLOY.equals(DEPLOY_SERVER)) {
 			e.printStackTrace();
 			return;
+		} else if (Settings.DEPLOY.equals(DEPLOY_DEVELOP)){
+			amfd.displayStackTrace(e);
 		}
-		StringWriter sw = new StringWriter();
-		e.printStackTrace(new PrintWriter(sw));
-		String stacktrace = sw.toString();
-		displayText(stacktrace, 1);
 	}
-
-//	public void loadIcon() {
-//		String icoPath = RepositoryPathsUtil.getStaticRepositoryPath() + File.separator + Settings.ICONS_DIR + File.separator + Settings.WEB_MAINFRAME_ICON;
-//		icoPath = icoPath.replaceAll("/", "\\\\");
-//		BufferedImage icoImg;
-//		try {
-//			icoImg = ImageIO.read(new File(icoPath));
-//			setIconImage(icoImg);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
 }
